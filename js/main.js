8fea5b8b6945372825be7895b3a3d03c975038f8
qrxt <46269438+qrxt@users.noreply.github.com>
2019-06-09 04:23:13 +0300

    Реализована часть отображения пинов

--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
+"use strict";
+
 var map = document.querySelector(".map");
+var mapPinsArea = map.querySelector(".map__pins");
 
 /* У блока .map уберем класс faded для перехода в активный режим */
 
-map.classList.remove("map--faded");
+map.classList.remove("map--faded"); // временно
 
 /* Сгенерируем массив, состоящий из 8 объектов объявлений */
 
 var housingTypes = ["palace", "flat", "house", "bungalo"];
 var announcements = [];
 
+var makeAvatarPathString = function (index) {
+  var expandedIndex = (index < 10) ? "0" + index : index;
+
+  return "img/avatars/user" + expandedIndex + ".png";
+};
+
 var generateAnnouncement = function (avatarIndex, type, x, y) {
   return {
     "author": {
-      "avatar": (avatarIndex < 10) ? "0" + avatarIndex : avatarIndex
+      "avatar": makeAvatarPathString(avatarIndex)
     },
     "offer": {
       type
     },
     "location": {
-      x, y
+      x: x + "px",
+      y: y + "px"
     }
   };
 };
@@ -33,6 +43,42 @@ var randomInteger = function (from, to) {
 
 for (var i = 1; i < 9; i++) {
   var currentType = getRandomArrayItem(housingTypes);
-  var currentX = 0;
-  var currentY = 0;
+  var currentX = randomInteger(0, mapPinsArea.offsetWidth);
+  var currentY = randomInteger(130, 630);
+
+  var announcement = generateAnnouncement(i, currentType, currentX, currentY);
+  announcements.push(announcement);
 };
+
+/* Создадим DOM-элементы на основе сгенерированных данных */
+
+var pinTemplate = document
+  .querySelector("#pin")
+  .content
+  .querySelector(".map__pin");
+
+var renderPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+  var pinElementCover = pinElement.querySelector("img");
+
+  pinElement.style.left = pin.location.x;
+  pinElement.style.top = pin.location.y;
+  pinElementCover.src = pin.author.avatar;
+  pinElementCover.alt = "asd";
+
+  return pinElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < announcements.length; i++) {
+  fragment.appendChild(renderPin(announcements[i]));
+}
+mapPinsArea.appendChild(fragment);
+
+/*
+
+  Проверить все ли аватарки задействуются
+  Проверить и дописать alt для пинов
+  Пофиксить неверные рамки местоположений пинов
+
+*/
