5b68fd22d76fe094c75af2a099717849f2fac286
qrxt <qrx357@gmail.com>
2019-06-09 15:03:32 +0300

    Фиксы по линтеру

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict';
 
-var map = document.querySelector(".map");
-var mapPinsArea = map.querySelector(".map__pins");
+var map = document.querySelector('.map');
+var mapPinsArea = map.querySelector('.map__pins');
 var pinSize = {
   width: 50,
   height: 70
-}
+};
 
 /* У блока .map уберем класс faded для перехода в активный режим */
 
-map.classList.remove("map--faded"); // временно
+map.classList.remove('map--faded'); // временно
 
 /* Сгенерируем массив, состоящий из 8 объектов объявлений */
 
-var housingTypes = ["palace", "flat", "house", "bungalo"];
+var housingTypes = ['palace', 'flat', 'house', 'bungalo'];
 var announcementsTitles = [
-  "Первое", "Второе", "Третье", "Четвертое",
-  "Пятое", "Шестое", "Седьмое", "Восьмое"
+  'Первое', 'Второе', 'Третье', 'Четвертое',
+  'Пятое', 'Шестое', 'Седьмое', 'Восьмое'
 ];
 
 var makeAvatarPathString = function (index) {
-  var expandedIndex = (index < 10) ? "0" + index : index;
+  var expandedIndex = (index < 10) ? '0' + index : index;
 
-  return "img/avatars/user" + expandedIndex + ".png";
+  return 'img/avatars/user' + expandedIndex + '.png';
 };
 
 var generateAnnouncement = function (avatarIndex, type, announcementTitle, x, y) {
   return {
-    "author": {
-      "avatar": makeAvatarPathString(avatarIndex),
-      announcementTitle
+    'author': {
+      'avatar': makeAvatarPathString(avatarIndex),
+      'announcementTitle': announcementTitle
     },
-    "offer": {
-      type
+    'offer': {
+      'type': type
     },
-    "location": {
-      x: x + "px",
-      y: y + "px"
+    'location': {
+      x: x + 'px',
+      y: y + 'px'
     }
   };
 };
@@ -50,7 +50,7 @@ function randomInteger(min, max) {
   return Math.floor(rand);
 }
 
-const fillAnnouncements = function () {
+var fillAnnouncements = function () {
   var announcements = [];
   for (var i = 1; i < 9; i++) {
     var currentType = getRandomArrayItem(housingTypes);
@@ -60,21 +60,21 @@ const fillAnnouncements = function () {
 
     var announcement = generateAnnouncement(i, currentType, currentTitle, currentX, currentY);
     announcements.push(announcement);
-  };
+  }
 
   return announcements;
-}
+};
 
 /* Создадим DOM-элементы на основе сгенерированных данных */
 
 var pinTemplate = document
-  .querySelector("#pin")
+  .querySelector('#pin')
   .content
-  .querySelector(".map__pin");
+  .querySelector('.map__pin');
 
 var renderPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
-  var pinElementCover = pinElement.querySelector("img");
+  var pinElementCover = pinElement.querySelector('img');
 
   pinElement.style.left = pin.location.x;
   pinElement.style.top = pin.location.y;
@@ -82,7 +82,7 @@ var renderPin = function (pin) {
   pinElementCover.alt = pin.author.announcementTitle;
 
   return pinElement;
-}
+};
 
 var announcements = fillAnnouncements();
 var fragment = document.createDocumentFragment();
