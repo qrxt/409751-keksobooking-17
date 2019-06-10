15cab85193789956fc711cdb04e12ed5a343874a
qrxt <qrx357@gmail.com>
2019-06-10 16:34:15 +0300

    Правки согласно кодгайду

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 var map = document.querySelector('.map');
 var mapPinsArea = map.querySelector('.map__pins');
-var pinSize = {
-  width: 50,
-  height: 70
+var PinSize = {
+  WIDTH: 50,
+  HEIGHT: 70
 };
 
 /* У блока .map уберем класс faded для перехода в активный режим */
@@ -13,11 +13,13 @@ map.classList.remove('map--faded'); // временно
 
 /* Сгенерируем массив, состоящий из 8 объектов объявлений */
 
-var housingTypes = ['palace', 'flat', 'house', 'bungalo'];
-var announcementsTitles = [
+var HOUSING_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ANNOUNCEMENTS_TITLES = [
   'Первое', 'Второе', 'Третье', 'Четвертое',
   'Пятое', 'Шестое', 'Седьмое', 'Восьмое'
 ];
+var MIN_AVAILABLE_Y = 130;
+var MAX_AVAILABLE_Y = 630;
 
 var makeAvatarPathString = function (index) {
   var expandedIndex = (index < 10) ? '0' + index : index;
@@ -45,18 +47,18 @@ var getRandomArrayItem = function (arr) {
   return arr[Math.round(Math.random() * (arr.length - 1))];
 };
 
-function randomInteger(min, max) {
+function getRandomInteger(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
 var fillAnnouncements = function () {
   var announcements = [];
-  for (var i = 1; i < 9; i++) {
-    var currentType = getRandomArrayItem(housingTypes);
-    var currentX = randomInteger(0, mapPinsArea.offsetWidth - pinSize.width);
-    var currentY = randomInteger(130, 630 - pinSize.height);
-    var currentTitle = announcementsTitles[i - 1];
+  for (var i = 1; i < ANNOUNCEMENTS_TITLES.length + 1; i++) {
+    var currentType = getRandomArrayItem(HOUSING_TYPES);
+    var currentX = getRandomInteger(0, mapPinsArea.offsetWidth - PinSize.WIDTH);
+    var currentY = getRandomInteger(MIN_AVAILABLE_Y, MAX_AVAILABLE_Y - PinSize.HEIGHT);
+    var currentTitle = ANNOUNCEMENTS_TITLES[i - 1];
 
     var announcement = generateAnnouncement(i, currentType, currentTitle, currentX, currentY);
     announcements.push(announcement);
@@ -86,7 +88,9 @@ var renderPin = function (pin) {
 
 var announcements = fillAnnouncements();
 var fragment = document.createDocumentFragment();
+
 for (var i = 0; i < announcements.length; i++) {
   fragment.appendChild(renderPin(announcements[i]));
 }
+
 mapPinsArea.appendChild(fragment);
