cb6c45377e0485749f0663a3a016feed4df44c88
qrxt <46269438+qrxt@users.noreply.github.com>
2019-07-18 03:49:48 +0300

    Теперь при ошибке получения данных с сервера при стабилизации соединения есть возможность получить данные без перезагрузки страницы

--- js/announcement.js
+++ js/announcement.js
@@ -36,126 +36,145 @@
 
   var announcementsUrl = 'https://js.dump.academy/keksobooking/data';
   var load = window.request('GET', announcementsUrl);
-  load(function (announcements) {
-    var getPinsFragment = function (currentAnnouncements) {
-      var fragment = document.createDocumentFragment();
-      currentAnnouncements
-        .slice(0, MAX_PINS_QUANTITY)
-        .forEach(function (announcement) {
-          var pin = new window.Pin(announcement);
-          var renderedPin = pin.render();
-          renderedPin.addEventListener('click', function () {
-            var card = new window.Card(announcement);
 
-            window.clearCardsArea();
-            mapPinsArea.appendChild(card.render());
+  var loadAnnouncements = function () {
+    load(function (announcements) {
+      var getPinsFragment = function (currentAnnouncements) {
+        var fragment = document.createDocumentFragment();
+        currentAnnouncements
+          .slice(0, MAX_PINS_QUANTITY)
+          .forEach(function (announcement) {
+            var pin = new window.Pin(announcement);
+            var renderedPin = pin.render();
+            renderedPin.addEventListener('click', function () {
+              var card = new window.Card(announcement);
+
+              window.clearCardsArea();
+              mapPinsArea.appendChild(card.render());
+            });
+            fragment.appendChild(renderedPin);
           });
-          fragment.appendChild(renderedPin);
-        });
 
-      return fragment;
-    };
-
-    /* Блокировка пина до загрузки данных с сервера */
-
-    mapPinMainText.innerHTML = window.initialMainPinText;
-    mapPinMain.disabled = false;
-
-    /* Фильтрация пинов */
-
-    var filterByType = function (data) {
-      var currentControlValue = housingTypeSelect.value;
-      if (currentControlValue === 'any') {
-        return data;
-      }
-      return data.filter(function (announcement) {
-        return announcement.offer.type === currentControlValue;
-      });
-    };
-
-    var filterByRooms = function (data) {
-      var currentControlValue = housingRoomsSelect.value;
-      if (currentControlValue === 'any') {
-        return data;
-      }
-      return data.filter(function (announcement) {
-        return announcement.offer.rooms === Number(currentControlValue);
-      });
-    };
-
-    var filterByPrice = function (data) {
-      var currentControlValue = housingPriceSelect.value;
-      var inPriceRangePredicates = {
-        'low': window.util.inRange(0, 9999),
-        'middle': window.util.inRange(10000, 49999),
-        'high': window.util.inRange(50000, MAX_ANNOUNCEMENT_PRICE),
+        return fragment;
       };
-      if (currentControlValue === 'any') {
-        return data;
-      }
-      return data.filter(function (announcement) {
-        return inPriceRangePredicates[currentControlValue](announcement.offer.price);
+
+      /* Блокировка пина до загрузки данных с сервера */
+
+      mapPinMainText.innerHTML = window.initialMainPinText;
+      mapPinMain.disabled = false;
+
+      /* Фильтрация пинов */
+
+      var filterByType = function (data) {
+        var currentControlValue = housingTypeSelect.value;
+        if (currentControlValue === 'any') {
+          return data;
+        }
+        return data.filter(function (announcement) {
+          return announcement.offer.type === currentControlValue;
+        });
+      };
+
+      var filterByRooms = function (data) {
+        var currentControlValue = housingRoomsSelect.value;
+        if (currentControlValue === 'any') {
+          return data;
+        }
+        return data.filter(function (announcement) {
+          return announcement.offer.rooms === Number(currentControlValue);
+        });
+      };
+
+      var filterByPrice = function (data) {
+        var currentControlValue = housingPriceSelect.value;
+        var inPriceRangePredicates = {
+          'low': window.util.inRange(0, 9999),
+          'middle': window.util.inRange(10000, 49999),
+          'high': window.util.inRange(50000, MAX_ANNOUNCEMENT_PRICE),
+        };
+        if (currentControlValue === 'any') {
+          return data;
+        }
+        return data.filter(function (announcement) {
+          return inPriceRangePredicates[currentControlValue](announcement.offer.price);
+        });
+      };
+
+      var filterByGuests = function (data) {
+        var currentControlValue = housingGuestsSelect.value;
+        if (currentControlValue === 'any') {
+          return data;
+        }
+        return data.filter(function (announcement) {
+          return announcement.offer.guests === Number(currentControlValue);
+        });
+      };
+
+      var filterByFeatures = function (data) {
+        var currentFeatures = Array
+          .from(housingFeatures)
+          .reduce(function (acc, feature) {
+            if (feature.checked) {
+              return acc.concat(feature.value);
+            }
+            return acc;
+          }, []);
+
+        return data.filter(function (announcement) {
+          return window.util.includesArr(announcement.offer.features, currentFeatures);
+        });
+      };
+
+      var applyAllFilters = window.util.pipe(filterByType, filterByRooms, filterByPrice, filterByGuests, filterByFeatures);
+
+      var drawFilteredAnnouncements = function () {
+        var filteredAnnouncements = applyAllFilters(announcements);
+
+        clearPinsArea();
+        window.clearCardsArea();
+        mapPinsArea.appendChild(getPinsFragment(filteredAnnouncements));
+      };
+
+      var debouncedDrawFilteredAnnouncements = window.util.debounce(drawFilteredAnnouncements, DEBOUNCE_INTERVAL);
+      var announcementsFilters = document.querySelectorAll('.map__filter, .map__checkbox');
+      announcementsFilters.forEach(function (filter) {
+        filter.addEventListener('change', function () {
+          debouncedDrawFilteredAnnouncements();
+        });
       });
-    };
 
-    var filterByGuests = function (data) {
-      var currentControlValue = housingGuestsSelect.value;
-      if (currentControlValue === 'any') {
-        return data;
-      }
-      return data.filter(function (announcement) {
-        return announcement.offer.guests === Number(currentControlValue);
-      });
-    };
+      /* Exports */
+      window.clearPinsArea = clearPinsArea;
+      window.initialPinsFragment = getPinsFragment(announcements);
+      window.drawFilteredAnnouncements = drawFilteredAnnouncements;
+    }, function () {
+      var errorElement = errorTemplate.cloneNode(true);
 
-    var filterByFeatures = function (data) {
-      var currentFeatures = Array
-        .from(housingFeatures)
-        .reduce(function (acc, feature) {
-          if (feature.checked) {
-            return acc.concat(feature.value);
-          }
-          return acc;
-        }, []);
+      var removeErrorElement = function () {
+        var currentError = main.querySelector('.error');
+        if (currentError) {
+          main.removeChild(currentError);
+        }
+      };
 
-      return data.filter(function (announcement) {
-        return window.util.includesArr(announcement.offer.features, currentFeatures);
-      });
-    };
+      var onErrorClick = function () {
+        removeErrorElement();
+        loadAnnouncements();
+      };
 
-    var applyAllFilters = window.util.pipe(
-        filterByType,
-        filterByRooms,
-        filterByPrice,
-        filterByGuests,
-        filterByFeatures
-    );
+      var onErrorEscKeydown = function (keydownEvt) {
+        if (keydownEvt.keyCode === window.util.KeyCodes.ESC) {
+          removeErrorElement();
+          loadAnnouncements();
+        }
+      };
 
-    var drawFilteredAnnouncements = function () {
-      var filteredAnnouncements = applyAllFilters(announcements);
+      errorElement.addEventListener('click', onErrorClick);
+      document.addEventListener('keydown', onErrorEscKeydown);
 
-      clearPinsArea();
-      window.clearCardsArea();
-      mapPinsArea.appendChild(getPinsFragment(filteredAnnouncements));
-    };
-
-    var debouncedDrawFilteredAnnouncements = window.util.debounce(drawFilteredAnnouncements, DEBOUNCE_INTERVAL);
-    var announcementsFilters = document.querySelectorAll('.map__filter, .map__checkbox');
-    announcementsFilters.forEach(function (filter) {
-      filter.addEventListener('change', function () {
-        debouncedDrawFilteredAnnouncements();
-      });
+      main.appendChild(errorElement);
     });
+  };
 
-    /* Exports */
-    window.clearPinsArea = clearPinsArea;
-    window.initialPinsFragment = getPinsFragment(announcements);
-    window.drawFilteredAnnouncements = drawFilteredAnnouncements;
-  }, function () {
-    var errorFragment = document.createDocumentFragment();
-    var errorElement = errorTemplate.cloneNode(true);
-
-    errorFragment.appendChild(errorElement);
-    main.appendChild(errorElement);
-  });
+  loadAnnouncements();
 })();
